cd02213920da55cd4cf26ddc4198603be9bede44
amims71 <amimulehshan29@gmail.com>
2020-03-17 17:21:48 +0600

    any date

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $months=[
     "স্বাধীনতা","শপথ","বেতারযুদ্ধ","যুদ্ধ","শোক","কৌশলযুদ্ধ",
     "আকাশযুদ্ধ","জেলহত্যা","বিজয়","ফিরে আসা","নবযাত্রা","ভাষা"];
-$date=date();
+$date=<your_date>;
 $old=strtotime($date."- 16 days -2 months");
 $day= date("d",$old);
 $month=$months[date("m",$old)-1];
